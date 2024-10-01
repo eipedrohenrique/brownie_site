35debb6a28e15c178f4f1a6065964a12a323b765
Pedro Henrique <92455469+eipedrohenrique@users.noreply.github.com>
2024-10-01 11:16:07 -0300

    projeto

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Seleciona o botão de compra
+const comprarBtn = document.getElementById('comprarBtn');
+
+// Adiciona um evento de clique no botão
+comprarBtn.addEventListener('click', () => {
+    // Redireciona para o iFood (ou outra página de vendas)
+    window.location.href = 'https://www.ifood.com.br';
+});
